4fcd05d6dad56987dbb47a7268b845906b0034c9
Anas Abubakar <anasabubakar2015@gmail.com>
2022-09-21 14:09:30 +0100

    Implement message decoding

--- index.rb
+++ index.rb
@@ -13,3 +13,10 @@ def decode_word(word)
   char_array = word.split
   char_array.map { |x| decode_char(x) }.join
 end
+
+def decode_msg(msg)
+  word_array = msg.split('   ')
+  word_array.map { |x| decode_word(x) }.join(' ')
+end
+
+puts decode_msg('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
